d1d39e30d521edd8003e6903683971f49b4b4faf
yaowang <appdev_yw@163.com>
2021-02-07 18:12:23 +0800

    crop代码

---
+++ common-widget/src/main/java/com/ywcommon/common/widgetlib/ucrop/callback/OverlayViewChangeListener.java
@@ -0,0 +1,13 @@
+package com.ywcommon.common.widgetlib.ucrop.callback;
+
+import android.graphics.RectF;
+
+/**
+ * Created by Oleksii Shliama.
+ */
+public interface OverlayViewChangeListener {
+
+    void onCropRectUpdated(RectF cropRect);
+
+    void postTranslate(float deltaX, float deltaY);
+}
